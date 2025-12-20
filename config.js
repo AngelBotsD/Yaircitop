006037b6d0ee24f7d5605150821df16d6c2bc8e2
AngelBotsD <angelperez22448@gmail.com>
2025-12-19 21:27:26 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -2,21 +2,27 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = ['159606034665538', '245573982662762','274135666176172', '217158512549931', '226044783132714', '25856038715509']
+global.owner = [
+  '159606034665538',
+  '245573982662762',
+  '274135666176172',
+  '217158512549931',
+  '226044783132714',
+  '25856038715509'
+]
 
 global.emoji = '📎'
 global.emoji2 = '🏞️'
 global.namebot = '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍'
 global.botname = '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍'
-global.banner
-'https://files.catbox.moe/igdrbi.jpg'
+global.banner = 'https://files.catbox.moe/igdrbi.jpg'
 global.packname = '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍'
 global.author = '𝖣𝖾𝗌𝖺𝗋𝗋𝗈𝗅𝗅𝖺𝖽𝗈 𝗉𝗈𝗋 𝖠𝗇𝗀𝖾𝗅'
 global.sessions = '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍'
 
-let file = fileURLToPath(import.meta.url)
+const file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
-  console.log(chalk.redBright("Se actualizo el 'config.js'"))
+  console.log(chalk.redBright("Se actualizó el 'config.js'"))
   import(`file://${file}?update=${Date.now()}`)
 })
