f5ef25dc511bd0a57400b996035fa8bd46580811
AngelBotsD <angelperez22448@gmail.com>
2025-12-19 20:13:54 -0600

    Actualizar brat.js

--- plugins/stickers/brat.js
+++ plugins/stickers/brat.js
@@ -3,31 +3,35 @@ import axios from "axios";
 const API_URL = "https://api-sky.ultraplus.click/brat";
 const API_KEY = process.env.API_KEY || "Angxlllll";
 
-let handler = async (m, { text }) => {
-  if (!text) return m.reply("✏️ Escribe un texto para usar brat");
+let handler = async (m, { conn, text }) => {
+  if (!text) {
+    return m.reply("✏️ Escribe un texto para usar *brat*");
+  }
 
   try {
-    const r = await axios.post(
+    const res = await axios.post(
       API_URL,
       { text },
-      { headers: { apikey: API_KEY } }
+      {
+        headers: { apikey: API_KEY },
+        responseType: "arraybuffer"
+      }
     );
 
-    console.log(r.data);
-
-    if (!r.data || !r.data.result)
-      return m.reply("❌ La API no devolvió texto");
-
-    await m.reply(r.data.result);
+    await conn.sendMessage(
+      m.chat,
+      { image: Buffer.from(res.data), caption: "✨ Brat" },
+      { quoted: m }
+    );
 
   } catch (e) {
     console.error(e);
-    m.reply("❌ Error usando el comando brat");
+    m.reply("❌ Error al generar el brat");
   }
 };
 
-handler.command = /^brat$/i;
 handler.help = ["brat <texto>"];
-handler.tags = ["texto"];
+handler.tags = ["maker"];
+handler.command = /^brat$/i;
 
 export default handler;
