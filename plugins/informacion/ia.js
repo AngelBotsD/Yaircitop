0693d61a2b9a0df4fab53bcb460edd7a3dcc61d9
AngelBotsD <angelperez22448@gmail.com>
2025-12-28 01:41:12 -0600

    Actualizar ia.js

--- plugins/informacion/ia.js
+++ plugins/informacion/ia.js
@@ -1,51 +1,24 @@
-import fetch from "node-fetch"
-
 let handler = async (m, { conn }) => {
 
+  if (!m.text) return
+
   const mentioned =
-    m.message?.extendedTextMessage?.contextInfo?.mentionedJid || []
+    m.text.startsWith("@") ||
+    m.text.toLowerCase().includes(conn.user.name.toLowerCase())
 
-  const isMention = mentioned.includes(conn.user.jid)
-  const isCommand = /^[\.]?(bot|gemini)/i.test(m.text)
+  if (!mentioned) return
 
-  if (!isMention && !isCommand) return
-
-  let text = m.text || ""
-
-  if (isMention) {
-    const botNumber = conn.user.jid.split("@")[0]
-    text = text.replace(new RegExp(`@${botNumber}`, "gi"), "").trim()
-  }
-
-  if (isCommand) {
-    text = text.replace(/^[\.]?(bot|gemini)\s*/i, "").trim()
-  }
+  let text = m.text
+    .replace(/^@\S+\s*/i, "")
+    .trim()
 
   if (!text) {
-    return m.reply(
-      "¡Hola!\nMi nombre es Bot\n¿En qué te puedo ayudar? ♥️"
-    )
+    return m.reply("hola si")
   }
 
-  try {
-    await conn.sendPresenceUpdate("composing", m.chat)
-
-    const apiUrl =
-      "https://apis-starlights-team.koyeb.app/starlight/gemini?text=" +
-      encodeURIComponent(text)
-
-    const res = await fetch(apiUrl)
-    const data = await res.json()
-
-    await m.reply(data.result || "🔴 Error en la API")
-  } catch (e) {
-    console.error(e)
-    await m.reply("❌ Error al procesar")
-  }
+  return m.reply("hola si")
 }
 
-handler.customPrefix = /^(\.?bot|\.?gemini)/i
+handler.customPrefix = /^@/i
 handler.command = new RegExp
-handler.tags = ["ai"]
-
 export default handler
