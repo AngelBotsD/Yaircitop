2257e1bb80a687a556a8e5743d18ea4b7a2d0873
AngelBotsD <angelperez22448@gmail.com>
2025-12-28 01:28:10 -0600

    Actualizar ia.js

--- plugins/informacion/ia.js
+++ plugins/informacion/ia.js
@@ -1,1 +1,10 @@
-//xd
+let handler = async (m, { conn }) => {
+  await conn.reply(m.chat, "hola si", m)
+}
+
+handler.all = async function (m) {
+  if (!m.mentionedJid || !m.mentionedJid.includes(this.user.jid)) return
+  await m.reply("hola si")
+}
+
+export default handler
